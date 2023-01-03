0340091d9bcfa85df43f370d1fb7d42e8eace635
3mr-5aled <moroamr2005@gmail.com>
2023-01-03 10:07:50 +0200

    cool transtion

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ function openCity(evt, cityName) {
   var i, tabcontent, tablinks, tabname
   tabcontent = document.querySelectorAll(".tabcontent")
   for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none"
+    // tabcontent[i].style.display = "none"
+    tabcontent[i].classList.remove("show")
+    tabcontent[i].classList.add("hidden")
   }
   tablinks = document.querySelectorAll(".tablinks")
   for (i = 0; i < tablinks.length; i++) {
@@ -12,7 +14,9 @@ function openCity(evt, cityName) {
   var elms = document.querySelectorAll(`[id='${tabname}']`)
   console.log("elms")
   for (var i = 0; i < elms.length; i++) {
-    elms[i].style.display = "flex"
+    // elms[i].style.display = "flex"
+    elms[i].classList.add("show")
+    elms[i].classList.remove("hidden")
   }
   evt.currentTarget.classList.add("active")
 }
